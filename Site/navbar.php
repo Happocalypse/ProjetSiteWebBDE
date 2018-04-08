93a3f56bde060eadaf0ba9bd783b5474a6b78a70
Happocalypse <maxime.neuville@viacesi.fr>
2018-04-08 12:18:30 +0200

    New files organisation + navbar included + page 'formsInscription.php'

---
+++ Site/navbar.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <title>navbar</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"/>
+        <link rel="stylesheet" href="CSS/navbar.css"/>
+    </head>
+    <body data-spy="scroll" data-target="#barmenu" data-o>
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="barmenu">
+                <a class="navbar-brand" href="#"><img src="images/bear-footprint.png" width="50" height="50" alt=""></a>
+                <div class="collapse navbar-collapse d-flex justify-content-center wrapper" id="navbarSupportedContent">
+                    <ul class="navbar-nav menuPrincipal">
+                        <li class="nav-item active link-wrapper">
+                            <a class="nav-link link sixth before after" href="#">Accueil <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item link-wrapper">
+                            <a class="nav-link link sixth before after" href="#">Staff</a>
+                        </li>
+                        <li class="nav-item link-wrapper">
+                            <a class="nav-link link sixth before after" href="#">Evenements</a>
+                        </li>
+                        <li class="nav-item link-wrapper">
+                            <a class="nav-link link sixth before after" href="#">Boite à idées</a>
+                        </li>
+                        <li class="nav-item link-wrapper">
+                            <a class="nav-link link sixth before after" href="#">Photos</a>
+                        </li>
+                        <li class="nav-item link-wrapper">
+                            <a class="nav-link link sixth before after" href="#">Boutique</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+        <?php
+
+
+
+        ?>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/3.0.0/less.min.js" ></script>
+    </body>
+</html>
