276889b587bec2dca09165f14b36f7bd89411303
Happocalypse <maxime.neuville@viacesi.fr>
2018-04-09 08:39:33 +0200

    Indentation Update

--- Site/navbar.php
+++ Site/navbar.php
@@ -1,38 +1,38 @@
-       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"/>
-        <link rel="stylesheet" href="CSS/navbar.css"/>
-    </head>
-    <body data-spy="scroll" data-target="#barmenu" data-o>
-        <div class="container">
-            <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="barmenu">
-                <a class="navbar-brand" href="#"><img src="images/bear-footprint.png" width="50" height="50" alt=""></a>
-                <div class="collapse navbar-collapse d-flex justify-content-center wrapper" id="navbarSupportedContent">
-                    <ul class="navbar-nav menuPrincipal">
-                        <li class="nav-item link-wrapper accueil">
-                            <a class="nav-link link sixth before after" href="index.php">Accueil</a>
-                        </li>
-                        <li class="nav-item link-wrapper staff">
-                            <a class="nav-link link sixth before after" href="staff.php">Staff</a>
-                        </li>
-                        <li class="nav-item link-wrapper evenements">
-                            <a class="nav-link link sixth before after" href="evenements.php">Evenements</a>
-                        </li>
-                        <li class="nav-item link-wrapper boiteAIdees">
-                            <a class="nav-link link sixth before after" href="boiteAIdees.php">Boite à idées</a>
-                        </li>
-                        <li class="nav-item link-wrapper photos">
-                            <a class="nav-link link sixth before after" href="photos.php">Photos</a>
-                        </li>
-                        <li class="nav-item link-wrapper boutique">
-                            <a class="nav-link link sixth before after" href="boutique.php">Boutique</a>
-                        </li>
-                        <li class="nav-item link-wrapper connexion">
-                            <a class="nav-link link sixth before after" href="formsConnexion.php">Connexion</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </div>
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-        <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/3.0.0/less.min.js" ></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"/>
+<link rel="stylesheet" href="CSS/navbar.css"/>
+</head>
+<body data-spy="scroll" data-target="#barmenu" data-o>
+    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="barmenu">
+            <a class="navbar-brand" href="#"><img src="images/bear-footprint.png" width="50" height="50" alt=""></a>
+            <div class="collapse navbar-collapse d-flex justify-content-center wrapper" id="navbarSupportedContent">
+                <ul class="navbar-nav menuPrincipal">
+                    <li class="nav-item link-wrapper accueil">
+                        <a class="nav-link link sixth before after" href="index.php">Accueil</a>
+                    </li>
+                    <li class="nav-item link-wrapper staff">
+                        <a class="nav-link link sixth before after" href="staff.php">Staff</a>
+                    </li>
+                    <li class="nav-item link-wrapper evenements">
+                        <a class="nav-link link sixth before after" href="evenements.php">Evenements</a>
+                    </li>
+                    <li class="nav-item link-wrapper boiteAIdees">
+                        <a class="nav-link link sixth before after" href="boiteAIdees.php">Boite à idées</a>
+                    </li>
+                    <li class="nav-item link-wrapper photos">
+                        <a class="nav-link link sixth before after" href="photos.php">Photos</a>
+                    </li>
+                    <li class="nav-item link-wrapper boutique">
+                        <a class="nav-link link sixth before after" href="boutique.php">Boutique</a>
+                    </li>
+                    <li class="nav-item link-wrapper connexion">
+                        <a class="nav-link link sixth before after" href="formsConnexion.php">Connexion</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/3.0.0/less.min.js" ></script>
--- Site/photos.php
+++ Site/photos.php
@@ -1,70 +1,70 @@
 <!DOCTYPE html>
 <html>
-	<head>
+    <head>
 
-		<meta charset="utf-8" />
-		<link rel="stylesheet" href="CSS/photos.css">
-		<title>Publication d'une photo</title>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="CSS/photos.css">
+        <title>Publication d'une photo</title>
         <?php include 'navbar.php' ?>
 
-	<?php
-		try
-		{
-			// On se connecte à MySQL
-			$bdd = new PDO('mysql:host=178.62.4.64;dbname=SiteBDEG1','groupeMN','1234');
-		}
-		catch(Exception $e)
-		{
-			// En cas d'erreur, on affiche un message et on arrête tout
-			die('Erreur : '.$e->getMessage());
-		}
+        <?php
+    try
+    {
+        // On se connecte à MySQL
+        $bdd = new PDO('mysql:host=178.62.4.64;dbname=SiteBDEG1','groupeMN','1234');
+    }
+        catch(Exception $e)
+        {
+            // En cas d'erreur, on affiche un message et on arrête tout
+            die('Erreur : '.$e->getMessage());
+        }
 
-		// On récupère le contenu du champ nom_evenement
-		$reponse = $bdd->query('SELECT (nom_evenement) FROM Evenement');
+        // On récupère le contenu du champ nom_evenement
+        $reponse = $bdd->query('SELECT (nom_evenement) FROM Evenement');
 
-	?>
+        ?>
 
-		<?php if($donnees = $reponse->fetch()){ ?>
+        <?php if($donnees = $reponse->fetch()){ ?>
 
-			<form method="post" action="script/result_add_photo.php" enctype="multipart/form-data">
+        <form method="post" action="script/result_add_photo.php" enctype="multipart/form-data">
 
-			<p>Titre de la photo :
-			<input type="text" name="title_image"/></p>
+            <p>Titre de la photo :
+                <input type="text" name="title_image"/></p>
 
-			<p> Veuillez choisir l'événement :
-			<select name='choix'>
+            <p> Veuillez choisir l'événement :
+                <select name='choix'>
 
-				<?php
+                    <?php
 
-				do {
-				?>
-					<option value="<?php echo $donnees['nom']; ?>"><?php echo $donnees['nom']; ?></option>
+    do {
+                    ?>
+                    <option value="<?php echo $donnees['nom']; ?>"><?php echo $donnees['nom']; ?></option>
 
-				<?php
+                    <?php
 
-				} while ($donnees = $reponse->fetch());
+    } while ($donnees = $reponse->fetch());
 
-			// Termine le traitement de la requête
-			$reponse->closeCursor();
-			?>
-			</select></p>
+    // Termine le traitement de la requête
+    $reponse->closeCursor();
+                    ?>
+                </select></p>
 
-			<p>Pièce Jointe
-			<input type="file" name="monfichier" /> <br />
-			Format accepté : png, jpeg et jpg <br />
-			Limite du fichier : 1 Mo
-			</p>
+            <p>Pièce Jointe
+                <input type="file" name="monfichier" /> <br />
+                Format accepté : png, jpeg et jpg <br />
+                Limite du fichier : 1 Mo
+            </p>
 
-			<input type="hidden" name="username" value="his_username" />
-			<p><input type="submit" value="Publier" /> </p>
+            <input type="hidden" name="username" value="his_username" />
+            <p><input type="submit" value="Publier" /> </p>
 
-			</form>
+        </form>
 
-		<?php
-		}else{
-			echo "<br/><br /><br /><h1>Vous ne pouvez pas publier une photo car il n'y a pas d'événement.</h1>";
-		}
-	?>
+        <?php
+}else{
+    echo "<br/><br /><br /><h1>Vous ne pouvez pas publier une photo car il n'y a pas d'événement.</h1>";
+}
+        ?>
 
-	</body>
+        </body>
 </html>
