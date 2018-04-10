11d2b62a0617a9a77e3e07e71391a9ddbffb8bba
CaptainMorgan7 <coste.morgan@hotmail.com>
2018-04-10 22:59:58 +0200

    Added "editButton.php/.css " and created the edit button

---
+++ Site/editButton.php
@@ -0,0 +1,11 @@
+
+<div id="yes" class="dropleft">
+	<button type="button" class="btn btn-primary" data-toggle="dropdown">
+    		Edition
+  		</button>
+	<div class="dropdown-menu">
+		<button class="dropdown-item" type="button" onclick="window.location='http://google.com'">Ajouter</button>
+		<button class="dropdown-item" type="button">Supprimer</button>
+		<button class="dropdown-item" type="button">Modifier</button>
+	</div>
+</div>
