7a07beb8f6960c9a700dcd7360b9097b89b3f1ca
Morgan <costemorgan1g@hotmail.com>
2018-04-10 13:19:46 +0200

    Design "boutique"

--- Site/boutique.php
+++ Site/boutique.php
@@ -1,14 +1,113 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="CSS/boutique.css">
-        <title>Boutique</title>
-        <?php include 'script/scriptBootStrapHead.php' ?>
-    </head>
-    <body>
-        <?php include 'navbar.php' ?>
-        <h1>Boutique</h1>
-        <?php include 'script/scriptBootStrapBody.php' ?>
-    </body>
+
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="CSS/boutique.css">
+    <title>Boutique</title>
+    <?php include 'script/scriptBootStrapHead.php' ?>
+</head>
+
+<body>
+    <?php include 'navbar.php' ?>
+    <h1>Boutique</h1>
+    <section>
+        <article>
+            <div id="shopCarousel" class="carousel slide carousel-fade" data-ride="carousel">
+                <!--Indicators-->
+                <ol class="carousel-indicators">
+                    <li data-target="#shopCarousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#shopCarousel" data-slide-to="1"></li>
+                    <li data-target="#shopCarousel" data-slide-to="2"></li>
+                </ol>
+                <!--/.Indicators-->
+                <!--Slides-->
+                <div class="carousel-inner" role="listbox">
+                    <div class="carousel-item active">
+                        <img src="images/slide1.jpg" alt="First slide">
+                        <div class="carousel-caption">
+                            <h3 class="h3-responsive">Light mask</h3>
+                            <p>First text</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="images/slide2.jpg" alt="Second slide">
+                        <div class="carousel-caption">
+                            <h3 class="h3-responsive">Strong mask</h3>
+                            <p>Secondary text</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="images/slide3.jpg" alt="Third slide">
+                        <div class="carousel-caption">
+                            <h3 class="h3-responsive">Slight mask</h3>
+                            <p>Third text</p>
+                        </div>
+                    </div>
+                </div>
+                <!--/.Slides-->
+                <!--Controls-->
+                <a class="carousel-control-prev" href="#shopCarousel" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+                <a class="carousel-control-next" href="#shopCarousel" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+                <!--/.Controls-->
+            </div>
+        </article>
+    </section>
+
+    <section>
+        <div class="card-deck" style="width: 85%;;margin: auto; margin-top: 100px;">
+            <div class="card">
+                <img class="card-img-top" src="images/pull.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Pull sexy</h5>
+                    <p class="card-text">Description</p>
+                    <button type="button" class="btn float-right btn-outline-primary" id="button_">Ajouter au panier</button>
+                </div>
+            </div>
+            <div class="card">
+                <img class="card-img-top" src="images/pull.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Tasse swag</h5>
+                    <p class="card-text">Description</p>
+                    <button type="button" class="btn float-right btn-outline-primary" id="button_" onclick="scadd_item.js">Ajouter au panier</button>
+                </div>
+            </div>
+            <div class="card">
+                <img class="card-img-top" src="images/pull.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Tasse swag</h5>
+                    <p class="card-text">Description</p>
+                    <button type="button" class="btn float-right btn-outline-primary" id="button_">Ajouter au panier</button>
+                </div>
+            </div>
+            <div class="card">
+                <img class="card-img-top" src="images/pull.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Pull sexy</h5>
+                    <p class="card-text">Description</p>
+                    <button type="button" class="btn float-right btn-outline-primary" id="button_">Ajouter au panier</button>
+                </div>
+            </div>
+            <div class="card">
+                <img class="card-img-top" src="images/pull.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Pull sexy</h5>
+                    <p class="card-text">Description</p>
+                    <button type="button" class="btn float-right btn-outline-primary" id="button_">Ajouter au panier</button>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+
+    <?php include 'script/scriptBootStrapBody.php' ?>
+</body>
+
 </html>
--- Site/navbar.php
+++ Site/navbar.php
@@ -3,25 +3,25 @@
             <a class="navbar-brand" href="#"><img src="images/bear-footprint.png" width="50" height="50" alt=""></a>
             <div class="collapse navbar-collapse d-flex justify-content-center wrapper" id="navbarSupportedContent">
                 <ul class="navbar-nav menuPrincipal">
-                    <li class="nav-item link-wrapper accueil">
+                    <li class="nav-item link-wrapper accueil yes">
                         <a class="nav-link link sixth before after" href="accueil.php">Accueil</a>
                     </li>
-                    <li class="nav-item link-wrapper staff">
+                    <li class="nav-item link-wrapper staff yes">
                         <a class="nav-link link sixth before after" href="staff.php">Staff</a>
                     </li>
-                    <li class="nav-item link-wrapper evenements">
+                    <li class="nav-item link-wrapper evenements yes">
                         <a class="nav-link link sixth before after" href="evenements.php">Evenements</a>
                     </li>
-                    <li class="nav-item link-wrapper boiteAIdees">
+                    <li class="nav-item link-wrapper boiteAIdees yes">
                         <a class="nav-link link sixth before after" href="boiteAIdees.php">Boite à idées</a>
                     </li>
-                    <li class="nav-item link-wrapper photos">
+                    <li class="nav-item link-wrapper photos yes">
                         <a class="nav-link link sixth before after" href="photos.php">Photos</a>
                     </li>
-                    <li class="nav-item link-wrapper boutique">
+                    <li class="nav-item link-wrapper boutique yes">
                         <a class="nav-link link sixth before after" href="boutique.php">Boutique</a>
                     </li>
-                    <li class="nav-item link-wrapper connexion">
+                    <li class="nav-item link-wrapper connexion yes">
                         <a class="nav-link link sixth before after" href="formsConnexion.php">Connexion</a>
                     </li>
                 </ul>
