ccbc8580c418f266ea463b9e954a151399591f39
Damien Watteau <damien.watteau@viacesi.fr>
2018-04-12 12:42:11 +0200

    Update photo

--- Site/photos.php
+++ Site/photos.php
@@ -12,8 +12,9 @@
     <header><?php include 'navbar.php';?></header>
 
 <body>
-    <div class="container">
 
+    <!-- Vérifier si l'utilisateur est membre du BDE -->
+    <a href="addPhoto.php" class="btn btn-primary btn-lg" role="button" aria-disabled="true" id="buttonAjouter">Ajouter une photo</a>
 
 
     <?php include('script/connexionBDD.php');
@@ -22,42 +23,54 @@
     $reponse=$bdd->query('SELECT (ID_evenement)FROM PARTICIPER WHERE ID_utilisateur=1');
     $data=$reponse->fetch();
 
-        if($data==NULL){
-            echo "<h1 class='my-4 text-center text-lg-left'>Vous avez participé à aucun événement.</h1>";
-        }else{
 
-            echo "<a href=\"addPhoto.php\" class=\"btn btn-primary btn-lg\" role=\"button\" aria-disabled=\"true\" id=\"buttonAjouter\">Ajouter une photo</a>";
+        if($data==NULL){
+            echo "<h1 class='my-4 text-center text-lg-left'>Il n'y a aucun événement</h1>";
+        }else{
 
             $i=0;
             $nom_evenements=array();
-        do{
-            array_push($nom_evenements,$data['ID_evenement']);
+            do{
+                array_push($nom_evenements,$data['ID_evenement']);
 
-        } while($data=$reponse->fetch());
-
-        $reponse->closeCursor();
-
-        echo '<pre>';
-        print_r ($nom_evenements);
-        echo '</pre>';
-
-        /* for($index=0;$index<sizeof($nom_evenements);$index++){
-
-            $reponse=$bdd->query('SELECT (url_image)FROM photos WHERE ID_evenement='. $nom_evenements[$index]);
-            $data=$reponse->fetch();
-
-                if($data==NULL AND $index=0){
-                    echo "Il y a aucune photo sur l'événénement.";
-                }else{
-                    do{
-                        echo '<br />'.$data['url_image'];
-                    } while ($data=$reponse->fetch());
-                }
+            } while($data=$reponse->fetch());
 
             $reponse->closeCursor();
+            ?><div class="container" style="float:left;"><?php
+
+            for($index=0;$index<sizeof($nom_evenements);$index++){
+                echo "<h1>Formulaire publication article</h1>";
+                echo "<div class=\"row\">";
+                $reponse=$bdd->query('SELECT (url_image)FROM photos WHERE ID_evenement='. $nom_evenements[$index]);
+                $data=$reponse->fetch();
+
+                    if($data == NULL){
+                        echo "Il y a aucune photo sur l'événénement.";
+                    }else{
+                        do{
+                            ?>
+                            <div class="col-md-3">
+                            <div class="thumbnail">
+                              <a href="<?php echo $data['url_image']; ?>">
+                                <img src="<?php echo $data['url_image']; ?>" alt="Lights" style="width:100%">
+                                <div class="caption">
+                                  <p>Lorem ipsum...</p>
+                                </div>
+                              </a>
+                            </div>
+                          </div>
+
+                         <?php
+                        } while ($data=$reponse->fetch());
+                        echo "</div>";
+                    }
+
+                $reponse->closeCursor();
+
+            }
 
         }
-    }*/
+
 
 
     ?>
