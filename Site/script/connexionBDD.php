bd803988f11abf75001a773fd4775711d200d323
Swatnes64 <robin.moncoutie@orange.fr>
2018-04-13 14:35:46 +0200

    utf8 dans le php

--- Site/script/connexionBDD.php
+++ Site/script/connexionBDD.php
@@ -1,6 +1,7 @@
 <?php
 try{
     $bdd = new PDO('mysql:host=178.62.4.64;dbname=Projet_BDE','groupeMN','1234');
+    $bdd->exec("set names utf8");
 }
 catch (PDOException $e) {
     print "Error!: " . $e->getMessage() . "<br/>";
