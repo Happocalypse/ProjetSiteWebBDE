81ffa238ffb7aede34423494e781da713ab4a50f
Swatnes64 <robin.moncoutie@orange.fr>
2018-04-15 10:54:19 +0200

    affichage idées

--- Site/boiteAIdees.php
+++ Site/boiteAIdees.php
@@ -3,54 +3,50 @@
     <head>
         <meta charset="utf-8">
         <link rel="stylesheet" href="CSS/boiteAIdees.css">
-        <title>Boite à idées</title>
+        <title>Staff</title>
         <?php include 'script/scriptBootStrapHead.php' ?>
     </head>
+
     <body>
         <?php include 'navbar.php' ?>
-        <h1 class="BAI" >Boite à Idées</h1>
-        <?php include 'script/scriptBootStrapBody.php' ?>
 
-    <div align="center">
-      <a class="prop" href="boiteAIdeesProp.php">Proposez vos idées</a>
-    </div>
+        <h1 class="BAI" >Boite à Idées</h1>
+        <div align="center">
+            <a class="prop" href="boiteAIdeesProp.php">Proposez vos idées</a><br /><br />
+        </div>
+
+
 
 
         <?php
-        /*mysql_connect("178.62.4.64", "groupeMN", "1234");
-        mysql_select_db("Projet_BDE");
+    include('script/connexionBDD.php');
 
-        $reponse = mysql_query("SELECT nom_evenement, description_evenement, date_evenement FROM evenements WHERE valide=0");
+        $readIdeas = $bdd->query('SELECT nom_evenement, description_evenement, date_evenement FROM evenements WHERE valide=0');
+        while( $ideas = $readIdeas->fetch()){
+
         ?>
 
-        <table>
-            <tr>
-                <td>nom</td>
-                <td>date</td>
-                <td>desc</td>
-            </tr>
-         <?php/
-            while($donnees = mysql_fetch_array($reponse))
-            {
-            ?>
-                <tr>
-                    <td><?php echo $donnees['nom_evenement'];?></td>
-                    <td><?php echo $donnees['date_evenement'];?></td>
-                    <td><?php echo $donnees['description_evenement'];?></td>
-                </tr>
-         <?php
-            }
-            mysql_close();*/
-            ?>
+
+        <form>
+            <div class="row">
+                <div class="col-md-2"></div>
+                <div class="col-md-8 idees">
+
+                    <h1><?php echo $ideas['nom_evenement']; ?></h1>
+
+                    <p class="date">Idée proposée le : <?php echo $ideas['date_evenement']; ?></p>
+
+                    <p class="description">
+                        <?php echo $ideas['description_evenement']; ?> </p>
+
+                </div>
+                <div class="col-md-2"></div>
+            </div>
+        </form>
 
 
 
-
-
-
-
-
-
+        <?php   } $readIdeas->closeCursor();?>
 
         <?php include 'script/scriptBootStrapBody.php' ?>
     </body>
