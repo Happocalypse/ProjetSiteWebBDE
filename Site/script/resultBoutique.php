4fa95f76c553c7484bfb5e1257d0d0d009b6bf47
Morgan <costemorgan1g@hotmail.com>
2018-04-16 15:59:34 +0200

    oui

--- Site/script/resultBoutique.php
+++ Site/script/resultBoutique.php
@@ -1,11 +1,10 @@
 <?php
 include('connexionBDD.php');
-
+session_start();
     if(isset($_POST['submitPanier']) and isset($_POST['idProduit'])){
 
         $sql= 'INSERT INTO PANIER (quantite, ID_produit, ID_utilisateur) VALUES (1,'.$_POST['idProduit'].','.$_SESSION['id'].')';
         $bdd->exec($sql);
-
     }
 
 header('Location: ../boutique.php');
