fc25efc2bd5051d037b5073d0f2f6e4b87c15c05
Happocalypse <maxime.neuville@viacesi.fr>
2018-04-16 16:44:46 +0200

    avancement accueil + admin tasks + modifications footer + assos + script connexions + fichier dlPdf

--- Site/accueil.php
+++ Site/accueil.php
@@ -9,11 +9,70 @@
     </head>
     <body>
         <?php include 'navbar.php' ?>
-        <h1>Accueil</h1>
-
-        <div id="discord">
-            <iframe src="https://discordapp.com/widget?id=401825719195402240&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0"></iframe>
-        </div>
+        <section id="intro">
+            <h1>Accueil</h1>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8">
+                        <h3>Plan</h3>
+                        <div id="map"></div>
+                    </div>
+                    <div class="col-md-4">
+                        <div id="discord">
+                            <iframe src="https://discordapp.com/widget?id=401825719195402240&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0"></iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="assos">
+            <div class="card-deck">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Jeux De Role</h5>
+                        <p class="card-text">On fait des JDR et parfois des jeux de plateaux. </p>
+                        <p class="card-text"><small class="text-muted">Président : Maxime Lavergne</small></p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Squash</h5>
+                        <p class="card-text">La seule Asso de Sport de école actuellement si tu veux casser le clicher de l'informaticien qui ne sort jamais et qui sais pas faire deux pas sans être éssouffler viens te défouler  au squash !</p>
+                        <p class="card-text"><small class="text-muted">Président : Robin Montcoutié</small></p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Crypto</h5>
+                        <p class="card-text">Excuses pour pouvoir partir le jeudi après-midi</p>
+                        <p class="card-text"><small class="text-muted">Président : Jean Lacadée</small></p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Hacking</h5>
+                        <p class="card-text">Le but est de pouvoir s'exercer à la pratique d'intrusions système, élevations de privillège, faille logiciel...</p>
+                        <p class="card-text"><small class="text-muted">Président : Maxime Neuville</small></p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <script>
+            function initMap() {
+                var pau = {lat: 43.317501, lng: -0.308964};
+                var map = new google.maps.Map(document.getElementById('map'), {
+                    zoom: 18,
+                    center: pau
+                });
+                var marker = new google.maps.Marker({
+                    position: pau,
+                    map: map
+                });
+            }
+        </script>
+        <script async defer
+                src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAtbpZd2fJeL9cJJ1SWGs6XRp-BzuL3ZU0&callback=initMap">
+        </script>
         <?php include 'footer.php' ?>
         <?php include 'script/scriptBootStrapBody.php' ?>
     </body>
---
+++ Site/assos.php
@@ -0,0 +1,48 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="CSS/assos.css">
+        <title>Accueil</title>
+        <?php include 'script/scriptBootStrapHead.php' ?>
+    </head>
+    <body>
+        <?php include 'navbar.php' ?>
+        <section>
+            <h1>ADMIN - Associations</h1>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Nom</th>
+                        <th scope="col">Président</th>
+                        <th scope="col">Description</th>
+                        <th scope="col">Options</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+    include 'script/connexionBDD.php';
+        function supprUsers($idCurrent){
+            $suppr = $bdd->prepare("");
+            $suppr->bindValue(':id',$idCurrent,PDO::PARAM_INT);
+            $suppr->execute();
+        }
+        $utilisateurs = $bdd->prepare("SELECT * FROM associations");
+        $utilisateurs->execute();
+
+        foreach($utilisateurs as $row){
+            echo '<form method="post" action="script/dlPDF.php"><th scope="row"><input type="text" readonly class="form-control-plaintext" name="id" value="'.$row["ID_association"].'"></th>'.
+                '<td>'.$row["nom_association"].'</td>'.
+                '<td>'.$row["president_association"].'</td>'.
+                '<td>'.$row["description_association"].'</td>'.
+                '<td><button type="submit" class="btn btn-danger">Telecharger</button></td></form>'.
+                '</tr>';
+        }
+                    ?>
+                </tbody>
+            </table>
+            <?php include 'footer.php' ?>
+            <?php include 'script/scriptBootStrapBody.php' ?>
+        </section>
+    </body>
+</html>
