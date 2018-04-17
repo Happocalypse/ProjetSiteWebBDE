092c225947e8eb69c16be0e0d4c7c8a40d903bff
Damien Watteau <damien.watteau@viacesi.fr>
2018-04-17 10:12:18 +0200

    Fix bug

--- Site/script/connexionBDD.php
+++ Site/script/connexionBDD.php
@@ -1,12 +1,9 @@
 <?php
 try{
-<<<<<<< HEAD
-    $bdd = new PDO('mysql:host=178.62.4.64;dbname=Projet_BDE2','groupeMN','1234');
-=======
+
     $bdd = new PDO('mysql:host=178.62.4.64;dbname=Projet_BDE_Final','groupeMN','1234');
->>>>>>> origin/master
-    $bdd2 = new PDO('mysql:host=178.62.4.64;dbname=Projet_BDE2','groupeMN','1234');
-    $bdd3 = new PDO('mysql:host=178.62.4.64;dbname=Projet_BDE2','groupeMN','1234');
+    $bdd2 = new PDO('mysql:host=178.62.4.64;dbname=Projet_BDE_Final','groupeMN','1234');
+    $bdd3 = new PDO('mysql:host=178.62.4.64;dbname=Projet_BDE_Final','groupeMN','1234');
     $bdd->exec("set names utf8");
 }
 catch (PDOException $e) {
