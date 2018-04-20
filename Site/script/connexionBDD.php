c119a7fb8aaff1b98b28c8d925ff9b6c94c00827
Happocalypse <maxime.neuville@viacesi.fr>
2018-04-20 09:51:31 +0200

    changement PDO

--- Site/script/connexionBDD.php
+++ Site/script/connexionBDD.php
@@ -1,9 +1,9 @@
 <?php
 try{
 
-    $bdd = new PDO('mysql:host=178.62.4.64;dbname=Projet_BDE_Final','groupeMN','1234');
-    $bdd2 = new PDO('mysql:host=178.62.4.64;dbname=Projet_BDE_Final','groupeMN','1234');
-    $bdd3 = new PDO('mysql:host=178.62.4.64;dbname=Projet_BDE_Final','groupeMN','1234');
+    $bdd = new PDO('mysql:host=localhost;dbname=Projet_BDE_Final','root','');
+    $bdd2 = new PDO('mysql:host=localhost;dbname=Projet_BDE_Final','root','');
+    $bdd3 = new PDO('mysql:host=localhost;dbname=Projet_BDE_Final','root','');
     $bdd->exec("set names utf8");
 }
 catch (PDOException $e) {
